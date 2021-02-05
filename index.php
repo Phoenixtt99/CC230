ad151c32da9685f7a4fa3ecb926b753b72886063
Phoenixtt99 <phoenixtrout@yahoo.com>
2021-02-05 15:58:55 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,51 @@
+<body>
+    <link rel ="stylesheet" href="css/about.css">
+
+    <div class="about"> 
+        <h1>Morgantown Exotics</h1>
+        <P>Welcom to Morgantown exotics. Here you can find many different types of exotic cars and motorcycls for sale.  </P>
+        </div>
+        <div clas="row">
+            <div class="column">
+                <div class="card">
+                    <img src="Images/1.jpeg">
+                    <div class="container">
+                        <h2>GSXR 600</h2>
+                        <p>Grey</p>
+                        <p>$8000</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="column">
+            <div class="card1">
+                <img src="Images/p2.jpeg">
+                <div class="container">
+                    <h2>BMW S1000 RR</h2>
+                    <p>White & Blue</p>
+                    <p>$12000</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card2">
+                <img src="Images/p3.jpeg">
+                <div class="container">
+                    <h2>Nissan GTR</h2>
+                    <p>White</p>
+                    <p>$180000</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card3">
+                <img src="Images/p4.jpeg">
+                <div class="container">
+                    <h2>Ferrari 488</h2>
+                    <p>Black</p>
+                    <p>$200000</p>
+                </div>
+            </div>
+        </div>
+</body>
