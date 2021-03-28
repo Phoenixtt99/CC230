0b88771302fcdfd5175674e97fc5188c4ca16c1a
Phoenixtt99 <78055164+Phoenixtt99@users.noreply.github.com>
2021-03-28 15:37:58 -0400

    Add files via upload

---
+++ cs230.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 23, 2021 at 08:50 PM
+-- Server version: 5.7.33-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs230`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `pid` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `descript` text NOT NULL,
+  `picpath` varchar(60) NOT NULL,
+  `upload_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`pid`, `title`, `descript`, `picpath`, `upload_date`, `rating`) VALUES
+(1, '1', '1', '../gallery/6053d936e4b816.22489352.jpeg', '2021-03-18 18:50:30', NULL),
+(2, '2', '2', '../gallery/6053d94d0e0831.13840247.jpg', '2021-03-18 18:50:53', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `pid` int(11) NOT NULL,
+  `Fname` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `profpic` varchar(50) NOT NULL DEFAULT '../images/default.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`pid`, `Fname`, `uname`, `profpic`) VALUES
+(1, 'qwe', 'qwe', '../profiles/603d6924453da1.65594658.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `revid` int(11) NOT NULL COMMENT 'id for each review',
+  `itemid` int(11) NOT NULL COMMENT 'id for item being reviewed',
+  `uname` varchar(80) NOT NULL COMMENT 'user reviewing item',
+  `title` varchar(60) NOT NULL,
+  `review-text` text NOT NULL,
+  `revdate` datetime NOT NULL,
+  `ratingnum` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT 'is there at least 1 review'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `Fname` varchar(50) NOT NULL,
+  `Lname` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `Fname`, `Lname`, `uname`, `email`, `password`) VALUES
+(1, 'qwe', 'qwe', 'qwe', 'qwe@qwe.com', '$2y$10$Yky8ZL597WUcJ/ynkF8mKeFx5WZp7Rs3UAJA/ZzfzDK2kcKI5ZF8.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`revid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `revid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id for each review';
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
